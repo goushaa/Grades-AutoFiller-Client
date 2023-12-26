1b00bbd072fdc09103be98b20ce5afa456cb1901
MustafaTarek77 <111199608+MustafaTarek77@users.noreply.github.com>
2023-12-26 04:17:33 +0200

    Styling the project

--- src/components/BubbleSheet.jsx
+++ src/components/BubbleSheet.jsx
@@ -1,70 +1,69 @@
-import React, { useState } from 'react';
-import '../CSS/BubbleSheet.css';
-import { useHistory } from 'react-router-dom'
-import { LinearProgress } from '@mui/material';
+import React, { useState } from "react";
+import "../CSS/BubbleSheet.css";
+import { useHistory } from "react-router-dom";
+import { LinearProgress } from "@mui/material";
 
 function BubbleSheet() {
-    const [loading, setLoading] = useState(false);
-    const history = useHistory();  // Create a history object using useHistory()
+  const [loading, setLoading] = useState(false);
+  const history = useHistory(); // Create a history object using useHistory()
 
+  const handleFormSubmit = async (event) => {
+    event.preventDefault();
 
-    const handleFormSubmit = async (event) => {
-        event.preventDefault();
+    // Show loading screen
+    setLoading(true);
 
-        // Show loading screen
-        setLoading(true);
+    // Make the POST request to the server
+    const formData = new FormData(event.target);
+    try {
+      const response = await fetch("http://localhost:5000/module2", {
+        method: "POST",
+        body: formData,
+      });
 
-        // Make the POST request to the server
-        const formData = new FormData(event.target);
-        try {
-            const response = await fetch("http://localhost:5000/module2", {
-                method: "POST",
-                body: formData,
-            });
+      console.log(response.ok);
 
-            console.log(response.ok);
+      if (response.ok) {
+        history.push("/success");
+      } else {
+        console.error("Server responded with an error:", response.status);
+      }
+    } catch (error) {
+      console.error("Error:", error);
 
-            if (response.ok) {
-                history.push('/success');
-            } else {
-                console.error('Server responded with an error:', response.status);
-            }
+      // Handle error if necessary
+    } finally {
+      // Hide loading screen
+      setLoading(false);
+    }
+  };
 
-        } catch (error) {
-            console.error('Error:', error);
-
-            // Handle error if necessary
-        } finally {
-            // Hide loading screen
-            setLoading(false);
-        }
-    };
-
-    return (
-        <div>
-            <div id="title">Add Bubble Sheer Here</div>
-            <div id="gradeDiv">
-                {loading ? (
-                    <LinearProgress />
-                ) : (
-                    <form onSubmit={handleFormSubmit} encType="multipart/form-data">
-                        <label>
-                            Bubble Sheet
-                            <input type="file" name="image" accept="image/*" />
-                        </label>
-                        <br />
-                        <label>
-                            Model Answer
-                            <input type="file" name="textFile" accept=".txt" />
-                        </label>
-                        <br />
-                        <button type="submit">Upload</button>
-                    </form>
-
-                )}
-            </div>
-        </div>
-    );
+  return (
+    <div>
+      <div id="title">Bubble Sheet Correction</div>
+      <div id="gradeDiv">
+        {loading ? (
+          <LinearProgress />
+        ) : (
+          <form onSubmit={handleFormSubmit} encType="multipart/form-data">
+            <label id="l">
+              Bubble Sheet Image
+              <input id="in1" type="file" name="image" accept="image/*" />
+            </label>
+            <br />
+            <label id="l">
+              Model Answer File
+              <input id="in2" type="file" name="textFile" accept=".txt" />
+            </label>
+            <br />
+            <button className="but" type="submit">
+              Upload
+            </button>
+          </form>
+        )}
+      </div>
+    </div>
+  );
 }
 
-export default BubbleSheet
+export default BubbleSheet;
--- src/components/GradeAutoFiller.jsx
+++ src/components/GradeAutoFiller.jsx
@@ -1,67 +1,67 @@
-import React, { useState } from 'react';
-import '../CSS/GradeAutoFiller.css';
-import { useHistory } from 'react-router-dom'
-import { LinearProgress } from '@mui/material';
+import React, { useState } from "react";
+import "../CSS/GradeAutoFiller.css";
+import { useHistory } from "react-router-dom";
+import { LinearProgress } from "@mui/material";
 
 function GradeAutoFiller() {
-    const [loading, setLoading] = useState(false);
-    const history = useHistory();  // Create a history object using useHistory()
+  const [loading, setLoading] = useState(false);
+  const history = useHistory(); // Create a history object using useHistory()
 
+  const handleFormSubmit = async (event) => {
+    event.preventDefault();
 
-    const handleFormSubmit = async (event) => {
-        event.preventDefault();
+    // Show loading screen
+    setLoading(true);
 
-        // Show loading screen
-        setLoading(true);
+    // Make the POST request to the server
+    const formData = new FormData(event.target);
+    try {
+      const response = await fetch("http://localhost:5000/module1", {
+        method: "POST",
+        body: formData,
+      });
 
-        // Make the POST request to the server
-        const formData = new FormData(event.target);
-        try {
-            const response = await fetch("http://localhost:5000/module1", {
-                method: "POST",
-                body: formData,
-            });
+      console.log(response.ok);
 
-            console.log(response.ok);
+      if (response.ok) {
+        history.push("/success");
+      } else {
+        console.error("Server responded with an error:", response.status);
+      }
+    } catch (error) {
+      console.error("Error:", error);
 
-            if (response.ok) {
-                history.push('/success');
-            } else {
-                console.error('Server responded with an error:', response.status);
-            }
+      // Handle error if necessary
+    } finally {
+      // Hide loading screen
+      setLoading(false);
+    }
+  };
 
-        } catch (error) {
-            console.error('Error:', error);
-
-            // Handle error if necessary
-        } finally {
-            // Hide loading screen
-            setLoading(false);
-        }
-    };
-
-    return (
-        <div>
-            <div id="title">Add Table Here</div>
-            <div id="gradeDiv">
-                {loading ? (
-                    <LinearProgress />
-
-
-                ) : (
-                    <form onSubmit={handleFormSubmit} encType="multipart/form-data">
-                        <input type="file" name="image" accept="image/*" />
-                        <label>
-                            <input type="checkbox" name="sendCheckbox" />
-                            Already-Made OCR
-                        </label>
-                        <button type="submit">Upload</button>
-                    </form>
-
-                )}
-            </div>
+  return (
+    <body>
+      <div>
+        <div id="title">Grades Sheet Correction</div>
+        <div id="gradeDiv">
+          {loading ? (
+            <LinearProgress />
+          ) : (
+            <form onSubmit={handleFormSubmit} encType="multipart/form-data">
+              <label id="l1">
+                Table Image
+                <input id="in3" type="file" name="image" accept="image/*" />
+                <label id="in4">Already-Made OCR</label>
+                <input id="in5" type="checkbox" name="sendCheckbox" />
+              </label>
+              <button className="but2" type="submit">
+                Upload
+              </button>
+            </form>
+          )}
         </div>
-    );
+      </div>
+    </body>
+  );
 }
 
 export default GradeAutoFiller;
